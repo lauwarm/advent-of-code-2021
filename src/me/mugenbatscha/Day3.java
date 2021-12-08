73a67a44042830b6b6d963c231a9cfa2ee9ba436
lauwarm <10065855+lauwarm@users.noreply.github.com>
2021-12-08 22:21:19 +0100

    updated Day3

--- src/me/mugenbatscha/Day3.java
+++ src/me/mugenbatscha/Day3.java
@@ -1,8 +1,20 @@
 package me.mugenbatscha;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class Day3 {
 	
-	public static int solution1() { return 0; }
+	public static int solution1() throws IOException { 
+		
+		Stream<String> stringLines = Files.lines(Paths.get("input_day2_1.txt"));
+
+	
+		return 0; 
+		
+	}
 	
 	public static int solution2() { return 0; }
 
