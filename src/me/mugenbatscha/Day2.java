2a029e2e799406542cba433a745a376c8ba535bb
lauwarm <10065855+lauwarm@users.noreply.github.com>
2021-12-08 22:05:05 +0100

    Day2 Part One

--- src/me/mugenbatscha/Day2.java
+++ src/me/mugenbatscha/Day2.java
@@ -1,12 +1,38 @@
 package me.mugenbatscha;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
 public class Day2 {
 	
-	public static int solution1() {
+
+	
+	public static int solution1() throws IOException {
 		
+		var wrapper_horizontal = new Object() { int horizontal = 0; };
+		var wrapper_depth = new Object() { int depth = 0; };
 		
+		Stream<String> stringLines = Files.lines(Paths.get("input_day2_1.txt"));
+
 		
-		return 0;
+		stringLines.forEach(l -> {
+			if(l.matches("forward(.*)")) {				
+				wrapper_horizontal.horizontal += Integer.parseInt(l.substring(8));
+			}
+			else if (l.matches("up(.*)")) {
+				wrapper_depth.depth -= Integer.parseInt(l.substring(3));
+			}
+			else if(l.matches("down(.*)")) {
+				wrapper_depth.depth += Integer.parseInt(l.substring(5));
+			}
+		});
+		
+		stringLines.close();
+
+		return wrapper_horizontal.horizontal * wrapper_depth.depth;
+		
 	}
 
 }
