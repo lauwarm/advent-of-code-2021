41a577e47368cded38e49267393f1a844fbbed8e
lauwarm <10065855+lauwarm@users.noreply.github.com>
2021-12-11 21:39:24 +0100

    day4 progress

--- src/me/mugenbatscha/Day4.java
+++ src/me/mugenbatscha/Day4.java
@@ -3,6 +3,7 @@ package me.mugenbatscha;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 public class Day4 {
@@ -16,10 +17,42 @@ public class Day4 {
 		 * put board into an array?
 		 * "marked" entries convert to 0
 		 * 10 possible win situations, 5 vert, 5 hori
-		 * 
+		 * 22 13 17 11  0
+		 * 8   2 23  4 24
+		 * 21  9 14 16  7
+		 *  6 10  3 18  5
+		 *  1 12 20 15 19
+		 *  ---
+		 *  22 13 16 11 0 8 2 23 4 24 21 9 14 16 7 6 10 3 18 5 1 12 20 15 19
+		 *  a____________ b__________ c___________ d__________ e____________
+		 *  a  b  c  d  e a b c  d e
+		 *  
+		 *  use a 2d array
+		 *  index for number of board
+		 *  
+		 *  iterate over array... set picked number to 0
+		 *  iterate over array... check if winning rule applies
+		 *  have a variable currentPickedNumber
 		 */
+		
+		var wrapper_drawNumbers = new Object() { int[] drawNumbers; };
+		var wrapper_boards = new Object() { int[] boards; };
+		var wrapper_currentNumber = new Object() { int currentNumber = 0; };
+		
+		String[] tmpArray;
+		
 		Stream<String> stringLines = Files.lines(Paths.get("input/input_day4.txt"));
 
+	//	stringLines.flatMap(x -> Arrays.stream(x.split(","))).forEach(System.out::println);
+		
+		tmpArray = stringLines.toArray(String[]::new);
+		
+		for (int a = 0; a < 100; a++) {
+			System.out.println(tmpArray[0].split(",")[a]);
+		}
+		
+//		System.out.println(tmpArray[0].split(",")[1]);
+	
 		stringLines.close();
 		
 		return 0;
