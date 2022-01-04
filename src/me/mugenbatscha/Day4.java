0caca33ffda35e0645f185d05bfae30d1f306322
lauwarm <10065855+lauwarm@users.noreply.github.com>
2022-01-04 22:43:29 +0100

    Day 4 finished

--- src/me/mugenbatscha/Day4.java
+++ src/me/mugenbatscha/Day4.java
@@ -14,41 +14,36 @@ public class Day4 {
 		var wrapper_boards = new Object() { int[] boards; };
 		var wrapper_currentNumber = new Object() { int currentNumber = 0; };
 		
-		String[] tmpArray;
+		String[] inputFile;
 		
 		Stream<String> stringLines = Files.lines(Paths.get("input/input_day4.txt"));
 
-		tmpArray = stringLines.toArray(String[]::new);
+		inputFile = stringLines.toArray(String[]::new);
+
 		wrapper_drawNumbers.drawNumbers = new int[100];//100,27
-
-		for (int a = 0; a < 100; a++) { //100,27
-			wrapper_drawNumbers.drawNumbers[a] = Integer.parseInt(tmpArray[0].split(",")[a]);
-		}
-
-		int leer = 0;
-		int emptyLine = 0;
-		int emptyNumber = 0;
 		wrapper_boards.boards = new int[2500]; //2500,75
 
+		for (int a = 0; a < 100; a++) { //100,27
+			wrapper_drawNumbers.drawNumbers[a] = Integer.parseInt(inputFile[0].split(",")[a]);
+		}
+
+		int singleDigit = 0;
+
 		for (int b = 2; b < 601; b++) { //601,19
-			if (tmpArray[b].length() == 0) {
-				emptyLine += 1;
+			if (inputFile[b].length() == 0) {
 			}
 			else {
-				for (int c = 0; c < tmpArray[b].split(" +").length; c++) {
-					if (tmpArray[b].split(" +")[c].length() == 0) {
-						emptyNumber += 1;
-					}
+				for (int c = 0; c < inputFile[b].split(" +").length; c++) {
+					if (inputFile[b].split(" +")[c].length() == 0) { }
 					else {
-						wrapper_boards.boards[leer] = Integer.parseInt(tmpArray[b].split(" +")[c]);
-						leer += 1;
+						wrapper_boards.boards[singleDigit] = Integer.parseInt(inputFile[b].split(" +")[c]);
+						singleDigit += 1;
 					}
 				}
 			}
 		}
 
-		int test = 0;
-		int temp = 0;
+		int winningIndex = 0;
 		for (int d = 0; d < wrapper_drawNumbers.drawNumbers.length; d++) {
 			System.out.println("-");
 			for (int e = 0; e < wrapper_boards.boards.length; e++) {
@@ -62,49 +57,52 @@ public class Day4 {
 								+ " " + wrapper_boards.boards[e + 4]);
 					}
 					if (e < 2496) {
-						if(((wrapper_boards.boards[e] == -1) &&
+						if (((wrapper_boards.boards[e] == -1) &&
 								(wrapper_boards.boards[e+1] == -1) &&
 								(wrapper_boards.boards[e+2] == -1) &&
 								(wrapper_boards.boards[e+3] == -1) &&
 								(wrapper_boards.boards[e+4] == -1))) {
 							System.out.println("WINNING INDEX?: " + e);
-							System.out.println("WINNING NUMBER?: " + wrapper_drawNumbers.drawNumbers[d]); //-1
-							test = e;
-							wrapper_boards.boards[e] = wrapper_drawNumbers.drawNumbers[d];
+							System.out.println("WINNING NUMBER?: " + wrapper_drawNumbers.drawNumbers[d-1]);
+							winningIndex = e;
 						}
 					}
-
+				}
+				if (e%25 == 0) {
 					if (e < 2480) { //2480,55
-						if(((wrapper_boards.boards[e] == -1) &&
-								(wrapper_boards.boards[e+5] == -1) &&
-								(wrapper_boards.boards[e+10] == -1) &&
-								(wrapper_boards.boards[e+15] == -1) &&
-								(wrapper_boards.boards[e+20] == -1))) {
-							System.out.println("WINNING INDEX?: " + e);
-							System.out.println("WINNING NUMBER?: " + wrapper_drawNumbers.drawNumbers[d]); //-1
-							test = e;
-							wrapper_boards.boards[e] = wrapper_drawNumbers.drawNumbers[d];
+						for (int f = 0; f < 4; f++) {
+							if(((wrapper_boards.boards[e+f] == -1) &&
+									(wrapper_boards.boards[e+f+5] == -1) &&
+									(wrapper_boards.boards[e+f+10] == -1) &&
+									(wrapper_boards.boards[e+f+15] == -1) &&
+									(wrapper_boards.boards[e+f+20] == -1))) {
+								System.out.println("WINNING INDEX?: " + e);
+								System.out.println("WINNING NUMBER?: " + wrapper_drawNumbers.drawNumbers[d-1]);
+								winningIndex = e;
+							}
 						}
+
 					}
 				}
+
 			}
-			if (test != 0) {
-				int test1 = test /25;
-				int boardStart = test1*25;
-				int ergebnis = 0;
-				System.out.println(boardStart);
+			if (winningIndex != 0) {
+				int winningBoard = winningIndex /25;
+				int boardStartIndex = winningBoard*25;
+				int result = 0;
+				System.out.println(boardStartIndex);
 				for (int i = 0; i < 25; i++) {
-					if (wrapper_boards.boards[boardStart] != -1) {
-						System.out.println("Number: " + wrapper_boards.boards[boardStart]);
-						ergebnis += wrapper_boards.boards[boardStart];
+					if (wrapper_boards.boards[boardStartIndex] != -1) {
+						System.out.println("Number: " + wrapper_boards.boards[boardStartIndex]);
+						result += wrapper_boards.boards[boardStartIndex];
 
 					}
-					boardStart++;
+					boardStartIndex++;
 				}
-				System.out.println("Ergebnis: " + ergebnis);
-				ergebnis = ergebnis * wrapper_drawNumbers.drawNumbers[d];
+				System.out.println("Result: " + result);
+				result = result * wrapper_drawNumbers.drawNumbers[d-1];
 
-				return ergebnis;
+				return result;
 			}
 		}
 
